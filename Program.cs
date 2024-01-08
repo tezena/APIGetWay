f2eff7308449d8dd02eb7572a7ec789d54b670e8
tezena <tewodrosnibretadal2012@gmail.com>
2024-01-08 04:14:21 -0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+//configure ocelot
+
+builder.Configuration.SetBasePath(builder.Environment.ContentRootPath).AddJsonFile("ocelot.json", optional: false, reloadOnChange: true).AddEnvironmentVariables();
+builder.Services.AddOcelot(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+
+
+await app.UseOcelot();
+
+app.Run();
+
